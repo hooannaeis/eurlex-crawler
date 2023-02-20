3ef9a4e5368422828e61ae352764fa83295b4d45
Hannes Kuhl <hannes.kuhl@017hamm7c9hrhh0.fritz.box>
2023-02-20 17:29:20 +0100

    feat: try/catch everywhere

--- src/utils/caseMeta.js
+++ src/utils/caseMeta.js
@@ -35,99 +35,158 @@ function getDocumentMetaSkelleton() {
 }
 
 function getCaseTitle(caseHtml) {
-    const TITLE_ID = "#title"
-    const titleNode = caseHtml.querySelector(TITLE_ID)
-    if (titleNode && titleNode.innerText) {
-        return titleNode.innerText.trim()
+    try {
+
+        const TITLE_ID = "#title"
+        const titleNode = caseHtml.querySelector(TITLE_ID)
+        if (titleNode && titleNode.innerText) {
+            return titleNode.innerText.trim()
+        }
+    } catch (e) {
+        return "default title"
     }
-    return "no title found"
 }
 
 function getCaseType(caseHtml) {
-    const typeNode = caseHtml.querySelector("#PPMisc_Contents")
-    if (typeNode && typeNode.innerText) {
-        const typeText = typeNode.innerText.trim()
-        const matches = [...typeText.matchAll(/Form:[\n\r\s]*(\w+)/gi)]
-        if (matches[0] && matches[0][1]) {
-            return matches[0][1]
+    try {
+
+        const typeNode = caseHtml.querySelector("#PPMisc_Contents")
+        if (typeNode && typeNode.innerText) {
+            const typeText = typeNode.innerText.trim()
+            const matches = [...typeText.matchAll(/Form:[\n\r\s]*(\w+)/gi)]
+            if (matches[0] && matches[0][1]) {
+                return matches[0][1]
+            }
         }
+    } catch (e) {
+        return "default caseType"
     }
-    return
 }
 
 function getCaseDate(caseHtml) {
-    const DATE_ID = "#PPDates_Contents"
-    const dateNode = caseHtml.querySelector(DATE_ID)
-    if (dateNode) {
-        const dateNodeText = dateNode.innerText.trim()
-        let matches = [...dateNodeText.matchAll(/Date of document:[\n\r\s]*(\d{1,2}\/\d{1,2}\/\d{4})/gmi)]
-        if (matches[0] && matches[0][1]) {
-            return matches[0][1]
+    try {
+        const DATE_ID = "#PPDates_Contents"
+        const dateNode = caseHtml.querySelector(DATE_ID)
+        if (dateNode) {
+            const dateNodeText = dateNode.innerText.trim()
+            let matches = [...dateNodeText.matchAll(/Date of document:[\n\r\s]*(\d{1,2}\/\d{1,2}\/\d{4})/gmi)]
+            if (matches[0] && matches[0][1]) {
+                return matches[0][1]
+            }
+            matches = [...dateNodeText.matchAll(/Date of vote:[\n\r\s]*(\d{1,2}\/\d{1,2}\/\d{4})/gmi)]
+            if (matches[0] && matches[0][1]) {
+                return matches[0][1]
+            }
+            return
         }
-        matches = [...dateNodeText.matchAll(/Date of vote:[\n\r\s]*(\d{1,2}\/\d{1,2}\/\d{4})/gmi)]
-        if (matches[0] && matches[0][1]) {
-            return matches[0][1]
-        }
-        return
+    } catch (e) {
+        return "default title"
     }
 }
 
+/**
+ * 
+ * @param {Object} caseHtml parsed case HTML as provisioned by axios
+ * @returns [Array] of strings
+ */
 function getCaseTags(caseHtml) {
-    const TAG_SELECTOR = "#PPClass_Contents li"
-    const tags = getListTextsBySelector(caseHtml, TAG_SELECTOR)
-    return tags
+    try {
+        const TAG_SELECTOR = "#PPClass_Contents li"
+        const tags = getListTextsBySelector(caseHtml, TAG_SELECTOR)
+        return tags
+    } catch (e) {
+        return []
+    }
 }
 
+/**
+ * 
+ * @param {Object} caseHtml parsed case HTML as provisioned by axios
+ * @returns [Array] of strings
+ */
 function getRawDocumentReferences(caseHtml) {
-    const TAG_SELECTOR = "#PPLinked_Contents li"
-    const tags = getListTextsBySelector(caseHtml, TAG_SELECTOR)
-    return tags
+    try {
+
+        const TAG_SELECTOR = "#PPLinked_Contents li"
+        const tags = getListTextsBySelector(caseHtml, TAG_SELECTOR)
+        return tags
+    } catch (e) {
+        return []
+    }
 }
 
+/**
+ * 
+ * @param {Object} caseHtml parsed case HTML as provisioned by axios
+ * @returns [Array] of strings
+ */
 function getTokenizedReferences(rawReferences) {
-    if (!rawReferences || !rawReferences.length || !rawReferences.push) { return [] }
-    const tokenizedReferences = []
-    rawReferences.forEach(ref => {
-        const celexID = getCelexIDFromString(ref)
-        const docSpecifiers = getDocumentSpecifierFromReference(ref)
-        tokenizedReferences.push([...celexID, ...docSpecifiers])
-    })
-    return tokenizedReferences
+    try {
+
+        if (!rawReferences || !rawReferences.length || !rawReferences.push) { return [] }
+        const tokenizedReferences = []
+        rawReferences.forEach(ref => {
+            const celexID = getCelexIDFromString(ref)
+            const docSpecifiers = getDocumentSpecifierFromReference(ref)
+            tokenizedReferences.push([...celexID, ...docSpecifiers])
+        })
+        return tokenizedReferences
+    } catch (e) {
+        return []
+    }
 }
 
 function getDocumentSpecifierFromReference(input) {
-    // matches a word  followed by a numberr. E. g. "paragraph 12" or "article 1"
-    const specifierRegex = /\b\w+\s\d{1,4}\b/g
-    const specifiers = getRegexMatchesFromString(input, specifierRegex)
-    if (specifiers && specifiers.length) return specifiers
-    return []
+    try {
+        // matches a word  followed by a numberr. E. g. "paragraph 12" or "article 1"
+        const specifierRegex = /\b\w+\s\d{1,4}\b/g
+        const specifiers = getRegexMatchesFromString(input, specifierRegex)
+        if (specifiers && specifiers.length) return specifiers
+        return []
+    } catch (e) {
+        return []
+    }
 }
 
 
 function getCelexIDFromString(input) {
-    // based on https://eur-lex.europa.eu/content/help/eurlex-content/celex-number.html
-    const celexRegex = /[\d|E|C][12]\d{3}[a-zA-Z]{1,2}\d{1,4}/g
-    const celexID = getRegexMatchesFromString(input, celexRegex)
-    if (celexID && celexID.length) return celexID
-    return []
+    try {
+        // based on https://eur-lex.europa.eu/content/help/eurlex-content/celex-number.html
+        const celexRegex = /[\d|E|C][12]\d{3}[a-zA-Z]{1,2}\d{1,4}/g
+        const celexID = getRegexMatchesFromString(input, celexRegex)
+        if (celexID && celexID.length) return celexID
+        return []
+    } catch (e) {
+        return []
+    }
 }
 
 function getRegexMatchesFromString(input, regex) {
-    const matches = input.match(regex)
-    if (matches) return matches
-    return undefined
+    try {
+
+        const matches = input.match(regex)
+        if (matches) return matches
+        return undefined
+    } catch (e) {
+        return undefined
+    }
 }
 
 function getListTextsBySelector(caseHtml, selector) {
-    const listItems = []
-    const itemNodes = caseHtml.querySelectorAll(selector)
-    if (itemNodes && itemNodes.length) {
-        itemNodes.forEach(itemNode => {
-            const cleanItemNode = cleanseString(itemNode.innerText.trim())
-            listItems.push(cleanItemNode)
-        })
+    try {
+
+        const listItems = []
+        const itemNodes = caseHtml.querySelectorAll(selector)
+        if (itemNodes && itemNodes.length) {
+            itemNodes.forEach(itemNode => {
+                const cleanItemNode = cleanseString(itemNode.innerText.trim())
+                listItems.push(cleanItemNode)
+            })
+        }
+        return listItems
+    } catch (e) {
+        return []
     }
-    return listItems
 }
 
 function cleanseString(str) {
