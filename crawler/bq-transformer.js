ef817f33cde9057ea311cfb3aa0f632b7dfd0eaa
Hannes Kuhl <hannes.kuhl@017hamm7c9hrhh0.fritz.box>
2023-03-13 21:26:45 +0100

    feat: bq-transformer more generic schema

--- crawler/bq-transformer.js
+++ crawler/bq-transformer.js
@@ -1,12 +1,12 @@
-;(async function () {
+; (async function () {
     const { writeFileSync, readFileSync } = require('fs')
-    const file = await readFileSync('eurlexdata.json')
+    const file = await readFileSync('eurLexLegalActs.json')
     const fileJson = JSON.parse(file)
 
     const newJson = []
     Object.keys(fileJson).forEach((itemKey) => {
         const entry = fileJson[itemKey]
-        newJson.push({ ...entry.caseMeta, caseBody: entry.caseBody })
+        newJson.push({ ...entry.meta, body: entry.body })
     })
-    writeFileSync('eurlexdata-bq.json', JSON.stringify(newJson))
+    writeFileSync('eurLexLegalActs-bq.json', JSON.stringify(newJson))
 })()
